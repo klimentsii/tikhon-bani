82ac1726c0249eed4e9f2bf04a0ff0dab39c54c4
klimentsii <klimparh@gmail.com>
2025-08-23 21:04:42 +0300

    feat: add contacts page and update home page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Catalog } from './pages/catalog/catalog';
 import { Equipment } from './pages/equipment/equipment';
+import { Contacts } from './pages/contacts/contacts';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'catalog', component: Catalog },
-  { path: 'equipment', component: Equipment }
+  { path: 'equipment', component: Equipment },
+  { path: 'contacts', component: Contacts }
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+  showCalculator = false;
+  phoneNumber = '';
+  selectedBathType = '';
 
+  calculatePrice() {
+    if (this.phoneNumber && this.selectedBathType) {
+      console.log('Заявка отправлена:', {
+        phone: this.phoneNumber,
+        bathType: this.selectedBathType
+      });
+      // Здесь будет логика отправки заявки
+      alert('Спасибо! Мы свяжемся с вами в ближайшее время.');
+      this.showCalculator = false;
+      this.phoneNumber = '';
+      this.selectedBathType = '';
+    } else {
+      alert('Пожалуйста, заполните все поля');
+    }
+  }
 }
