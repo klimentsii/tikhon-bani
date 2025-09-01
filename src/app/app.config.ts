9de078a3439641c1bbc48f8713a183aec10865e3
klimentsii <klimparh@gmail.com>
2025-09-01 23:16:16 +0300

    feat: added http and metric

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient()
   ]
 };
