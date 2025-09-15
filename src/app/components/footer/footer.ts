3eac495bfd7e18f6c843ada7dec465afe0b72c12
klimentsii <klimparh@gmail.com>
2025-09-15 21:17:05 +0300

    feat: added map to footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+declare var ymaps: any;
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +10,62 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
-export class Footer { }
+export class Footer implements OnInit, AfterViewInit {
+  
+  ngOnInit() {
+    // Проверяем, загружена ли библиотека Яндекс карт
+    if (typeof ymaps !== 'undefined') {
+      this.initMap();
+    } else {
+      // Если библиотека еще не загружена, ждем её загрузки
+      this.waitForYmaps();
+    }
+  }
+
+  ngAfterViewInit() {
+    // Дополнительная проверка после инициализации компонента
+    if (typeof ymaps !== 'undefined' && !this.isMapInitialized()) {
+      this.initMap();
+    }
+  }
+
+  private waitForYmaps() {
+    const checkYmaps = () => {
+      if (typeof ymaps !== 'undefined') {
+        this.initMap();
+      } else {
+        setTimeout(checkYmaps, 100);
+      }
+    };
+    checkYmaps();
+  }
+
+  private isMapInitialized(): boolean {
+    return document.getElementById('yandex-map')?.hasChildNodes() || false;
+  }
+
+  private initMap() {
+    if (this.isMapInitialized()) {
+      return; // Карта уже инициализирована
+    }
+
+    ymaps.ready(() => {
+      // Координаты Березовского района, Брестская область
+      const map = new ymaps.Map('yandex-map', {
+        center: [52.689344, 25.322071], // Точные координаты BANYA TUT
+        zoom: 10,
+        controls: ['zoomControl', 'fullscreenControl']
+      });
+
+      // Добавляем маркер
+      const placemark = new ymaps.Placemark([52.689344, 25.322071], {
+        balloonContent: 'BANYA TUT<br/>Брестская область, Березовский район',
+        hintContent: 'BANYA TUT'
+      }, {
+        preset: 'islands#greenDotIcon'
+      });
+
+      map.geoObjects.add(placemark);
+    });
+  }
+}
